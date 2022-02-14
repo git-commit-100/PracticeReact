a30bd3bd0fa7ef581e6a00e0125e918dbb17beec
Java Monk <javamonk.coder@gmail.com>
2022-02-14 18:03:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Card from "./components/Layout/Card";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Card>
-        <h2>Lets get started</h2>
-      </Card>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-//*to animate any changes in the layout of the dom inside app, just have to add a "layout" prop
-import { AnimateSharedLayout } from "framer-motion";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AnimateSharedLayout>
-      <App />
-    </AnimateSharedLayout>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
